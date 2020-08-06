85b1c08f97ca7f22baf408dca2882b3b0b2d35dc
boris <boris@example.com>
2020-08-06 12:31:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import DropDown from './components/DropDown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = [
+  {
+    label: 'color is red',
+    value: 'red'
+  },
+  {
+    label: 'color is blue',
+    value: 'blue'
+  },
+  {
+    label: 'color is black',
+    value: 'black'
+  }
+]
+
+const items = [
+  {
+    title: 'What is React',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React',
+    content: 'React is a favorite javascript framework'
+  },
+  {
+    title: 'How do you use React',
+    content: 'You use React by creating components'
+  }
+]
+
+export default () => {
+
+  const [selected, setSelected] = useState(options[0]);
+
+  return <div>
+    <Header />
+    <Route path="/" >
+      <Accordion items={items} />
+    </Route>
+    <Route path="/list" >
+      <Search />
+    </Route>
+    <Route path="/dropdown" >
+    <DropDown
+      selected={selected}
+      options={options}
+      onSelectedChange={setSelected}
+    />
+    </Route>
+    <Route path="/translate" >
+    <Translate />
+    </Route>
+  </div>
 }
-
-export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Accordion = ({items}) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+const onTitleClick = (key) => {
+    setActiveIndex(key)
+}
+
+const renderedItems = items.map((item, key) => {
+const active = key === activeIndex ? 'active' : '';
+    return <React.Fragment key={item.title}>
+        <div className={`title ${active}`}
+        onClick={() => {onTitleClick(key)}}
+        >
+            <i className="dropdown icon"></i>
+            {item.title}
+        </div>
+        <div className={`content ${active}`}>
+            <p>{item.content}</p>
+        </div>
+    </React.Fragment>
+})
+
+    return <div className="ui styled accordion">{renderedItems}</div>
+}
+
+export default Accordion;
